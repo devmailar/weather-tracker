f74f1a8ad5cefe8124642318761b9e750d2bf141
Mailar <leo.s.koivunen@gmail.com>
2022-02-23 15:07:38 +0200

    Add files via upload

---
+++ assets/js/weather.js
@@ -0,0 +1,35 @@
+let weather = {
+  apiKey: "b9d5cad3404a8e36451f44bb37dcab7e",
+
+  fetchWeather: function (city) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    // Log out our information about weather
+    console.log(name, icon, description, temp, humidity, speed);
+
+    document.querySelector(".city").innerText = name;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temperature").innerText = "Temperature: " + temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+})
+
+document.querySelector(".city").innerText = "Example";
